bf427fc5625e72a9a678461b70ca8ff6a8c9469f
Ромашко <romashko-bor@mail.ru>
2024-04-22 20:50:03 +0300

    add test v3

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -20,11 +20,11 @@ public class CreditAccountTest {
 
     @Test
     public void addToNegativeBalance() {
-        CreditAccount account = new CreditAccount(1000, 5_000, 15);
+        CreditAccount account = new CreditAccount(-1000, 5_000, 15);
 
         account.add(3_000);
 
-        Assertions.assertEquals(4_000, account.getBalance());
+        Assertions.assertEquals(2_000, account.getBalance());
     }
 
     @Test
@@ -72,12 +72,12 @@ public class CreditAccountTest {
         CreditAccount account = new CreditAccount(0, 500, 15);
         account.add(1000);
 
-        Assertions.assertEquals(false, account.pay(0));
+        Assertions.assertEquals(1000, account.pay(0));
     }
 
     @Test
     public void amountLessThanLimit() {
-        CreditAccount account = new CreditAccount(0, 5000, 15);
+        CreditAccount account = new CreditAccount(0, 5_000, 15);
         account.add(1000);
 
         Assertions.assertEquals(true, account.pay(5_000));
@@ -85,7 +85,7 @@ public class CreditAccountTest {
 
     @Test
     public void amountExceedsLimit() {
-        CreditAccount account = new CreditAccount(0, 5000, 15);
+        CreditAccount account = new CreditAccount(0, 5_000, 15);
         account.add(1000);
 
         Assertions.assertEquals(false, account.pay(10_000));
@@ -107,7 +107,7 @@ public class CreditAccountTest {
 
     @Test
     public void addCreditLimit() {
-        CreditAccount account = new CreditAccount(-1000, 5000, 15);
+        CreditAccount account = new CreditAccount(-1000, 5_000, 15);
 
         Assertions.assertEquals(5_000, account.getCreditLimit());
     }
@@ -115,9 +115,9 @@ public class CreditAccountTest {
 
     @Test
     public void payExceedingLimitCredit() {
-        CreditAccount account = new CreditAccount(1000, 5000, 15);
-        account.pay(3000);
+        CreditAccount account = new CreditAccount(1000, 5_000, 15);
+        account.pay(8_000);
 
-        Assertions.assertEquals(-2000, account.getBalance());
+        Assertions.assertEquals(1000, account.getBalance());
     }
 }
